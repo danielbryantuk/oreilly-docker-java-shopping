97f4797bac9ef11584018b9c4d14cc24c4663438
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-10-03 15:29:21 +0100

    Complete basic REST-based tests

---
+++ resttests/src/test/java/uk/co/taidev/springshopping/resttests/ShopfrontIntegrationTest.java
@@ -0,0 +1,45 @@
+package uk.co.taidev.springshopping.resttests;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+
+public class ShopfrontIntegrationTest {
+
+    private static final ContentType CONTENT_TYPE = ContentType.JSON;
+
+    private static final String SUT_BASE_URI = "http://localhost:8010/";
+
+    @Test
+    public void correctNumberOfProductsReturned() {
+        given().contentType(CONTENT_TYPE)
+                .when()
+                .get(SUT_BASE_URI + "products")
+                .then()
+                .body("size()", is(5));
+    }
+
+    @Test
+    public void productOneHasCorrectProductInfo() {
+        given()
+                .contentType(CONTENT_TYPE)
+                .when()
+                .get(SUT_BASE_URI + "products")
+                .then()
+                .body("[0].id", is("1"))
+                .body("[0].sku", is("12345678"));
+    }
+
+    @Test
+    public void productOneHasCorrectStockInfo() {
+        given()
+                .contentType(CONTENT_TYPE)
+                .when()
+                .get(SUT_BASE_URI + "products")
+                .then()
+                .body("[0].amountAvailable", is(5));
+    }
+}
