4e00eed8882ed0f173c83967699c7280369452a0
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-10-03 11:24:32 +0100

    Add basic REST-driven tests

---
+++ resttests/src/test/java/uk/co/taidev/springshopping/resttests/entities/Product.java
@@ -0,0 +1,49 @@
+package uk.co.taidev.springshopping.resttests.entities;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Integer id;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer amountAvailable;
+
+
+    public Product() {
+    }
+
+    public Product(Integer id, String sku, String name, String description, BigDecimal price, Integer amountAvailable) {
+        this.id = id;
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.amountAvailable = amountAvailable;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getAmountAvailable() {
+        return amountAvailable;
+    }
+}
